92c4080e9f11eec3d0bc18f27af616bcb315159f
Vanshita <vanshita.2226cse1063@kiet.edu>
2023-08-23 20:35:59 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 const App = () => (
    <main className="relative">
     <nav>
-      <section className="xl:padding-1">
-
+      <section className="xl:padding-1 wide:padding-r padding-b">
+       Hero
       </section>
     </nav>
-      
+      <section className="padding">
+        PopularProducts
+      </section>
    </main>
 );
 
