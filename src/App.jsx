76b378cd3b4b2551a58cbd3eee9ab187391d8fc0
Vanshita <vanshita.2226cse1063@kiet.edu>
2023-08-23 20:31:48 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+const App = () => (
+   <main className="relative">
+    <nav>
+      <section className="xl:padding-1">
+
+      </section>
+    </nav>
+      
+   </main>
+);
+
+export default App;
