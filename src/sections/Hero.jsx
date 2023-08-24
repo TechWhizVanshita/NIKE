aa7c3f59358d928da259e50738620afbd85be7f2
Vanshita <vanshita.2226cse1063@kiet.edu>
2023-08-24 15:29:06 +0530

    add

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,8 @@
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section id="home" className="w-full border-2 flex xl:flex-row flex-col border-red-500 p-2 justify-center min-h-screen gap-10 max-container">
+
+    </section>
   )
 }
 
