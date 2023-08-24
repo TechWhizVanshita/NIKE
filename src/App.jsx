8ea38d50114e134039650aa7b7a429676ad2510e
Vanshita <vanshita.2226cse1063@kiet.edu>
2023-08-24 12:47:23 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -20,16 +20,16 @@ const App = () => (
         SpecialOffers
       </section>
 
-      <section className="padding">
-        SpecialOffers
+      <section className="padding bg-pale-blue">
+        CustomerReviews
+      </section>
+
+      <section className="padding-x sm:py-32 py-16 w-full">
+        Subscribe
       </section>
 
       <section className="padding">
-        SpecialOffers
-      </section>
-
-      <section className="padding">
-        SpecialOffers
+        Footer
       </section>
 
       
