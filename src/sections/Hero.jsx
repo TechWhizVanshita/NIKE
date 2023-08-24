261b76c05b29f20ac358fbc2b80cd909cc63a308
Vanshita <vanshita.2226cse1063@kiet.edu>
2023-08-24 15:50:30 +0530

    add

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,16 @@
 const Hero = () => {
   return (
-    <section id="home" className="w-full border-2 flex xl:flex-row flex-col border-red-500 p-2 justify-center min-h-screen gap-10 max-container">
-
+    <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
+     <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+      <p>Our Summer Collections</p>
+      <h1>
+          <span>The New Arrival</span>
+          <br />
+          <span>Nike </span>
+           Shoes
+        </h1>
+        <p>Discover stylish Nike arrivals quality <br />comfort, and innovation for your active life.</p>
+     </div>
     </section>
   )
 }
