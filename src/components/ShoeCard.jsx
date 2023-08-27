674e47d91847fda43c029f0d9d70084cf0a7b513
Vanshita <vanshita.2226cse1063@kiet.edu>
2023-08-27 16:15:23 +0530

    add

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -7,20 +7,21 @@ const ShoeCard = ({ imgURL, changeBigShoeImage, bigShoeImg }) => {
 
   return (
     <div
-      className={`border-2 rounded-xl ${
+      className={`border-2 rounded-xl 
+      ${
         bigShoeImg === imgURL.bigShoe
           ? "border-coral-red"
           : "border-transparent"
       } cursor-pointer max-sm:flex-1`}
       onClick={handleClick}
     >
-      <div className='flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4'>
+      <div className="flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4">
         <img
           src={imgURL.thumbnail}
-          alt='shoe colletion'
+          alt="shoe colletion"
           width={127}
           height={103.34}
-          className='object-contain'
+          className="object-contain"
         />
       </div>
     </div>
