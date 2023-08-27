3a2df0499d07dc721b186cd1a48846275c1ba7cf
Vanshita <vanshita.2226cse1063@kiet.edu>
2023-08-27 18:34:51 +0530

    add

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,7 +1,25 @@
+import Button from "../components/Button";
+
 const SuperQuality = () => {
   return (
-    <div>SuperQuality</div>
-  )
-}
+    <section
+      id="about-us"
+      className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
+    >
+      <div className="flex flex-1 flex-col">
+        <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          We Provide You
+          <span className="text-coral-red"> Super</span>
+          <span className="text-coral-red"> Quality</span> Shoes
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Ensuring premium comfort and style, our meticulously crafted footwear is designed to elevate your experience, providing you with unmatched quality, innovation and a touch of elegance.
+        </p>
 
-export default SuperQuality
+        <Button label="View details" />
+      </div>
+    </section>
+  );
+};
+
+export default SuperQuality;
