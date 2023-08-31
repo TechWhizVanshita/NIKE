ccd83869e101891c6504c0706bcc8eef4fe60c05
Vanshita <vanshita.2226cse1063@kiet.edu>
2023-08-31 23:48:18 +0530

    add

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,4 +1,6 @@
+import { copyrightSign } from "../assets/icons";
 import { footerLogo } from "../assets/images";
+import { footerLinks, socialMedia } from "../constants";
 
 const Footer = () => {
   return (
@@ -6,11 +8,56 @@ const Footer = () => {
       <div className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-col">
         <div className="flex flex-col items-start">
           <a href="/">
-            <img src={footerLogo} width={150} height={46}/>
+            <img src={footerLogo} width={150} height={46} />
           </a>
-          <p>Get shoes ready for the new term at your nearest Nike store. Find Your perfect Size In Store. Get Rewards</p>
+          <p className="text-white-400 sm:max-w-sm mt-6 text-base leading-7 font-montserrat">
+            Get shoes ready for the new term at your nearest Nike store. Find
+            Your perfect Size In Store. Get Rewards
+          </p>
+          <div className="flex items-center gap-5 mt-8">
+            {socialMedia.map((icon) => (
+              <div className="flex justify-center items-center w-12 h-12 bg-white rounded-full cursor-pointer">
+                <img src={icon.src} alt={icon.alt} width={24} height={24} />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="flex flex-1 justify-between lg:gap-10 gap-20 flex-wrap">
+          {footerLinks.map((section) => (
+            <div key={section}>
+              <h4 className="text-white text-2xl font-montserrat font-medium leading-normal mb-6 cursor-pointer">
+                {section.title}
+              </h4>
+              <ul className="text-white-400 ">
+                {section.links.map((link) => (
+                  <li
+                    className="mt-3 font-montserrat text-base leading-normal hover:text-slate-gray cursor-pointer"
+                    key={link.name}
+                  >
+                    <a>{link.name}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
         </div>
       </div>
+
+      <div className="flex text-white-400 mt-24 max-sm:flex-col max-sm:items-center">
+        <div className="flex flex-1 justify-start items-center gap-2 font-montserrat cursor-pointer">
+          <img 
+           src={copyrightSign}
+           alt="copy right"
+           width={20}
+           height={20}
+          //  className="rounded-full m-0"
+          />
+          <p>Copyrights. All rights reserved.</p>
+        </div>
+        <p className="font-montserrat cursor-pointer">Terms & Conditions</p>
+      </div>
+
+
     </footer>
   );
 };
